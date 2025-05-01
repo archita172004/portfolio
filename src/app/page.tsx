483da8b8f6ed3fae3140c542bb5ba5aa3925e4ce
archita172004 <archichaudhary2004@gmail.com>
2025-05-01 19:28:45 +0530

    2nd commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import "./globals.css";
 export default function Home() {
   return (
-    <div className="bg-slate-400">
-      <h1>portfolio</h1>
+    <div>
+      <div className="relative w-full h-screen overflow-hidden flex items-center justify-center">
+        {/* Tilted gray background */}
+        <div className="absolute inset-0 z-0 bg-gray-300 clip-diagonal"></div>
+
+        {/* Content layer (z-10 to go above background) */}
+        <div className="relative z-10 flex w-full h-full">
+          {/* Left side */}
+          <div className="w-1/2 flex flex-col justify-center items-start pl-20">
+            <h2 className="text-xl font-medium m-4">Hi, I am</h2>
+            <h1 className="text-5xl font-bold mt-2">Archita Chaudhary</h1>
+            <p className="text-gray-500 mt-1 font-semibold">
+              Front-end Developer
+            </p>
+          </div>
+
+          {/* Right side */}
+          <div className="w-1/2 bg-black flex items-end justify-center">
+            <img
+              src="/your-image.png"
+              alt="Profile"
+              className="h-[80%] object-contain"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,60 @@
+"use client";
+
 import React from "react";
+import Link from "next/link";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="flex justify-end p-4 ">
-      <div className="flex flex-row gap-4 items-center">
-        <div>About</div>
-        <div>Skills</div>
-        <div>Portfolio</div>
-        <div>Experiences</div>
-        <div>
-          <button>CONTACT ME</button>
-        </div>
+    <nav className="bg-black text-white flex items-center justify-end py-4 px-6 font-mono">
+      <div className=" hidden md:flex items-center space-x-8 ">
+        <Link href="#about"> About</Link>
+        <Link href="#skills">Skills</Link>
+        <Link href="#portfolio">Portfolio</Link>
+        <Link href="Experiences">Experiences</Link>
+        <Link
+          href="#contact"
+          className="bg-white text-black px-4 py-2 rounded-full font-medium hover:bg-gray-200 transition "
+        >
+          CONTACT ME
+        </Link>
       </div>
+
+      <div className="md:hidden z-50 " onClick={toggleMenu}>
+        {isMenuOpen ? <IoMdClose size={28} /> : <RxHamburgerMenu size={24} />}
+      </div>
+
+      {isMenuOpen && (
+        <div className="fixed inset-0 bg-black/90 flex flex-col items-center justify-center space-y-5 text-xl md:hidden z-40">
+          <Link href="#about" onClick={toggleMenu}>
+            About me
+          </Link>
+          <Link href="#skills" onClick={toggleMenu}>
+            Skills
+          </Link>
+          <Link href="#portfolio" onClick={toggleMenu}>
+            Portfolio
+          </Link>
+          <Link href="#expeirence" onClick={toggleMenu}>
+            Experience
+          </Link>
+          <Link
+            href="#contact"
+            onClick={toggleMenu}
+            className="bg-white text-black px-4 py-2 rounded-full font-medium hover:bg-gray-200 transition "
+          >
+            CONTACT ME
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
